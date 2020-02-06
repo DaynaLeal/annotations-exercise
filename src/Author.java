a2db7e42a0f55e5d8b47c341fe23161c1a3efd14
Dayna Leal <dayna.r.leal@gmail.com>
2020-02-06 15:06:19 -0600

    suppressed all warnings successfully

--- src/Author.java
+++ src/Author.java
@@ -3,9 +3,10 @@
  */
 import java.util.ArrayList;
 import java.util.List;
-
+@SuppressWarnings({"deprecation", "unchecked"})
 public class Author extends Person {
     private List books;
+
 
     public Author(String firstName, String lastName) {
         super(firstName, lastName);
